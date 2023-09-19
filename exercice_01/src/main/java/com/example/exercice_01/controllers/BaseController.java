1903d4d4cfd54a3ab16aa6b0827a007b86403408
aurelie661 <boutaurel59@gmail.com>
2023-09-19 10:49:54 +0200

    initiation 2

--- exercice_01/src/main/java/com/example/exercice_01/controllers/BaseController.java
+++ exercice_01/src/main/java/com/example/exercice_01/controllers/BaseController.java
@@ -5,13 +5,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@RequestMapping("/base")
+@RequestMapping("/base") // http://localhost:8080/base
 public class BaseController {
-    @GetMapping("1-param")
-    public String getOneParam(Model model){
+
+    // @RequestMapping(method = RequestMethod.GET)
+    @GetMapping("1-param") // GET http://localhost:8080/base/1-param
+    public String get1Param(Model model) {
         model.addAttribute("paramA", "John DUPONT");
 
         return "1-param";
+    }
+
+    @GetMapping("2-params") // GET http://localhost:8080/base/2-params
+    public String get2Params(Model model) {
+        model.addAttribute("paramA", "John DUPONT");
+        model.addAttribute("paramB", "Maria SMITH");
+
+        return "2-params";
+    }
+
+    @GetMapping("coll-params") // GET http://localhost:8080/base/coll-params
+    public String getCollParams(Model model) {
+        /*model.addAttribute("params", new ArrayList<String>());*/
+        model.addAttribute("params", List.of("John DUPONT", "Maria SMITH", "Chloée MARTEZ"));
+
+        return "dossier/coll-params";
     }
 }
