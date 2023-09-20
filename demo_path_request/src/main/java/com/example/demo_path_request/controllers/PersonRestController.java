f7876a68432f2e829d558a0818fb92406dfde326
aurelie661 <boutaurel59@gmail.com>
2023-09-20 12:11:08 +0200

    exo3 in progress

--- demo_path_request/src/main/java/com/example/demo_path_request/controllers/PersonRestController.java
+++ demo_path_request/src/main/java/com/example/demo_path_request/controllers/PersonRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/api/v1/persons")
+@RequestMapping("/persons")
 @RestController
 @RequiredArgsConstructor
 public class PersonRestController {
