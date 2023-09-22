4b0fb8b0bfbd2d00b4e2ff9d311eca73f4707cda
aurelie661 <boutaurel59@gmail.com>
2023-09-22 12:25:28 +0200

    update form contact

---
+++ demo_path_request/src/main/java/com/example/demo_path_request/controllers/PersonRestController.java
@@ -0,0 +1,38 @@
+package com.example.demo_path_request.controllers;
+
+import com.example.demo_path_request.exeptions.ResourceNotFoundException;
+import com.example.demo_path_request.models.PersonDTO;
+import com.example.demo_path_request.services.PersonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/persons") // http://localhost:8080/api/persons
+public class PersonRestController {
+    private final PersonService personService;
+
+    @GetMapping("list") // GET http://localhost:8080/api/persons/list
+    public List<PersonDTO> listPersons() {
+        return personService.getPersons();
+    }
+
+    @GetMapping("details/{personId}") // GET http://localhost:8080/api/persons/details/XXXXX
+    public ResponseEntity<Optional<PersonDTO>> getPersonById(@PathVariable("personId") UUID id) {
+        Optional<PersonDTO> found = personService.getPersonById(id);
+
+        if (found.isPresent()) {
+            return ResponseEntity.ok(found);
+        } else {
+            throw new ResourceNotFoundException();
+        }
+    }
+}
