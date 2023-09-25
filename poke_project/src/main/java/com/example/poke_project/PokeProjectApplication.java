2d648886ce763cc6d4e531a01cfa027d30500e91
aurelie661 <boutaurel59@gmail.com>
2023-09-25 11:45:56 +0200

    start poke

---
+++ poke_project/src/main/java/com/example/poke_project/PokeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.poke_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeProjectApplication.class, args);
+    }
+
+}
---
+++ poke_project/src/test/java/com/example/poke_project/PokeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poke_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
