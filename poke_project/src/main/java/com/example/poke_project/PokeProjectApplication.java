c76980a01b1cebde213b09fd3620c2bd04395552
aurelie661 <boutaurel59@gmail.com>
2023-09-25 15:14:18 +0200

    start poke

--- poke_project/src/main/java/com/example/poke_project/PokeProjectApplication.java
+++ poke_project/src/main/java/com/example/poke_project/PokeProjectApplication.java
@@ -2,12 +2,14 @@ package com.example.poke_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PokeProjectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PokeProjectApplication.class, args);
     }
-
 }
